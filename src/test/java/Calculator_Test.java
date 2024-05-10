1d621cd8111ab5409a7c8a31783d27f51d483ec0
yusuf10-prog <kuzkayayusuf404@gmail.com>
2024-05-11 00:37:07 +0300

    11.05.24

--- src/test/java/Calculator_Test.java
+++ src/test/java/Calculator_Test.java
@@ -4,6 +4,7 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
@@ -25,25 +26,22 @@ public class Calculator_Test {
         desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"10.0");
         desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
         desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
-        desiredCapabilities.setCapability(MobileCapabilityType.APP,"/Users/yusuf/IdeaProjects/Appium_113/Apps/Calculator_8.6 (612662282)_APKPure (1).apk");
+       // desiredCapabilities.setCapability(MobileCapabilityType.APP,"/Users/yusuf/IdeaProjects/Appium_113/Apps/Calculator_8.6 (612662282)_APKPure (1).apk");
 
         driver=new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"),desiredCapabilities);
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
+        driver.activateApp("com.google.android.calculator");
 
-        /*
-        DesiredCapabilities capabilities=new DesiredCapabilities();
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"Pixel 2");
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"10.0");
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
-        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
-        // capabilities.setCapability(MobileCapabilityType.UDID,"ea3e6f63");
-        capabilities.setCapability(MobileCapabilityType.APP,"/Users/yusuf/IdeaProjects/Appium_113/Apps/Calculator_8.6 (612662282)_APKPure (1).apk");
-        // capabilities.setCapability(MobileCapabilityType.APP, "/Users/yusuf/IdeaProjects/Appium_T120/Apps/Apk Bilgisi_2.3.4_apkcombo.com (1).apk");
 
-        driver=new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
-        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        Assert.assertTrue(driver.isAppInstalled("com.google.android.calculator"));
 
-         */
+        // uygulamanin acildigini dogrular
+        AndroidElement acButonu=driver.findElementByAccessibilityId("clear");
+        Assert.assertTrue(acButonu.isDisplayed());
+
+        // carpma, bolme, toplama ve cikarma islemleri yaparak sonuclari dogrular
+
+        driver.findElementByAccessibilityId("");
     }
 }
